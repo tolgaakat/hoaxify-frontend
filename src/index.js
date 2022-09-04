a2e8851606e5997ddfb9e412dca819ab6dbdc696
TURKCELL\TCTAKAT <tolga.akat@turkcell.com.tr>
2022-09-04 10:49:55 +0300

    Bolum4_35 end

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UserSignupPage from './pages/UserSignupPage';
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,7 +1,9 @@
 import React from "react";
-import axios from "axios";
 import { signup } from "../api/apiCalls";
 import Input from "../components/input";
+import { withTranslation } from 'react-i18next';
+import i18n from "../i18n";
+import { changeLanguage } from "i18next";
 
 
 class UserSignupPage extends React.Component {
@@ -16,15 +18,16 @@ class UserSignupPage extends React.Component {
     }
 
     onChange = event => {
+        const { t } = this.props;
         const { name, value } = event.target;
         const errors = { ...this.state.errors }; // kopyalama -spread operator
         errors[name] = undefined
 
         if (name === 'password' || name === 'passwordRepeat') {
-            if (name === 'password' && value != this.state.passwordRepeat) {
-                errors.passwordRepeat = 'Password mismatch!';
-            } else if (name === 'passwordRepeat' && value != this.state.password) {
-                errors.passwordRepeat = 'Password mismatch!';
+            if (name === 'password' && value !== this.state.passwordRepeat) {
+                errors.passwordRepeat = t('Password mismatch');
+            } else if (name === 'passwordRepeat' && value !== this.state.password) {
+                errors.passwordRepeat = t('Password mismatch');
             } else {
                 errors.passwordRepeat = undefined;
             }
@@ -77,12 +80,16 @@ class UserSignupPage extends React.Component {
 
     }
 
-
+    onChangeLanguage = language => {
+        const { i18n } = this.props;
+        i18n.changeLanguage(language);
+        changeLanguage(language); //axios  accept-language header prm. set eder
+    }
 
 
     render() {
 
-
+        const { t } = this.props;
         const { pendingApiCall, errors } = this.state;
         const { username, displayName, password, passwordRepeat } = errors;
 
@@ -91,29 +98,33 @@ class UserSignupPage extends React.Component {
 
             <div className="container">
                 <form>
-                    <h1 className="text-center">Sign Up</h1 >
-                    <Input name="username" label="Username" error={username}
+                    <h1 className="text-center">{this.props.t('Sign Up')}</h1 >
+                    <Input name="username" label={t("Username")} error={username}
                         onChange={this.onChange} />
                     {/* <div className="form-group">
                         <label>Username</label>
                         <input className={username ? "form-control is-invalid" : "form-control"} name="username" onChange={this.onChange} />
                         <div className="invalid-feedback">{username} </div> 
                     </div> */}
-                    <Input name="displayName" label="Display Name" error={displayName}
+                    <Input name="displayName" label={t("Display Name")} error={displayName}
                         onChange={this.onChange} />
-                    <Input name="password" label="Password" error={password}
+                    <Input name="password" label={t("Password")} error={password}
                         onChange={this.onChange} type="password" />
-                    <Input name="passwordRepeat" label="Password Repeat" error={passwordRepeat}
+                    <Input name="passwordRepeat" label={t("Password Repeat")} error={passwordRepeat}
                         onChange={this.onChange} type="password" />
                     <div className="text-center">
                         <button
                             className="btn btn-primary"
                             onClick={this.onClickSignup}
-                            disabled={pendingApiCall || passwordRepeat != undefined}
+                            disabled={pendingApiCall || passwordRepeat !== undefined}
                         >
                             {pendingApiCall && <span className="spinner-border spinner-border-sm"> </span>}
-                            Sign Up
+                            {t('Sign Up')}
                         </button>
+                    </div>
+                    <div>
+                        <img src="https://cdn.countryflags.com/thumbs/turkey/flag-square-250.png" width="24" height="24" alt="Turkish Flag" onClick={() => this.onChangeLanguage('tr')} style={{ cursor: 'pointer' }}></img>
+                        <img src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-square-250.png" width="24" height="24" alt="USA Flag" onClick={() => this.onChangeLanguage('en')} style={{ cursor: 'pointer' }}></img>
                     </div>
                 </form >
             </div >
@@ -121,4 +132,6 @@ class UserSignupPage extends React.Component {
     }
 }
 
-export default UserSignupPage;
+const UserSignupPageWithTranslation = withTranslation()(UserSignupPage);
+
+export default UserSignupPageWithTranslation;
