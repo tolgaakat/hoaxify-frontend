99d807b115ccfc49c4c09627eadb3480150a10d4
TURKCELL\TCTAKAT <tolga.akat@turkcell.com.tr>
2022-09-07 22:08:34 +0300

    Bolum5_49 end

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -7,38 +7,63 @@ class LoginPage extends Component {
 
     state = {
         username: null,
-        password: null
+        password: null,
+        error: null
     }
 
     onChange = event => {
         const { name, value } = event.target;
         this.setState({
-            [name]: value
+            [name]: value,
+            error: null
         })
     }
 
-    onClickLogin = event => {
+    onClickLogin = async event => {
         event.preventDefault();
 
         const { username, password } = this.state;
         const creds = {
             username,
             password
+        };
+
+        this.setState({
+            error: null
+        });
+
+        try {
+            await login(creds);
+        }
+        catch (apiError) {
+            this.setState(
+                {
+                    error: apiError.response.data.message
+                }
+            )
         }
 
-        login(creds);
     }
 
     render() {
         const { t } = this.props;
+
+        const { username, password, error } = this.state;
+
+        const buttonEnabled = username && password;
+
         return (
             <div className='container'>
                 <form>
                     <h1 className='text-center'>{t("Login")}</h1>
                     <Input label={t("Username")} name="username" onChange={this.onChange}></Input>
                     <Input label={t("Password")} name="password" type="password" onChange={this.onChange}></Input>
+                    {error && <div className="alert alert-danger">
+                        {error}
+                    </div>}
                     <div className='text-center'>
-                        <button className='btn btn-primary' onClick={this.onClickLogin}>Login</button>
+                        <button className='btn btn-primary' onClick={this.onClickLogin}
+                            disabled={!buttonEnabled}>Login</button>
                     </div>
                 </form>
             </div>
